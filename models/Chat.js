b70cd94d088bbecfa0b831a1d2323254d5416387
CharlesGPT <chimecharles23@gmail.com>
2025-11-24 12:01:16 -0500

    Created all by routes for the backend

---
+++ models/Chat.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const ChatSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        message: [
+            {
+                role: {type: String, required: true},
+                content: {type: String, required: true},
+                timestamp: {type: Number, required: true},
+            },
+        ],
+        userId: {type: String, required: true},
+    },
+    {timestamps: true}
+);
+
+const Chat = mongoose.models.Chat || mongoose.model("User", ChatSchema)
+
+export default Chat;
